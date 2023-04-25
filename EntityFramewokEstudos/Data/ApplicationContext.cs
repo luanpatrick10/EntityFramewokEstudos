f110ae05f1d58e285415c116240aecf9ee150a57
luanpatrick10 <86279613+luanpatrick10@users.noreply.github.com>
2023-04-24 23:54:08 -0300

    Pacotes.

---
+++ EntityFramewokEstudos/Data/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramewokEstudos.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MyInstance;Integrated Security=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
